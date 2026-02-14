9773e6c3ba0c561ce9e1802bb72d876b65a19fe5
Code Ronin <programmingadev@gmail.com>
2026-02-14 14:36:02 -0800

    add item to cart

--- projects/e-commerce-api/app.py
+++ projects/e-commerce-api/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 
 login_manager = LoginManager()
 app = Flask(__name__)
@@ -19,6 +19,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=True)
+    cart = db.relationship('CartItem', backref='user', lazy=True)
 
 
 class Product(db.Model):
@@ -26,6 +27,12 @@ class Product(db.Model):
     name = db.Column(db.String(120), nullable=False)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=False)
+
+
+class CartItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
 
 
 @login_manager.user_loader
@@ -153,6 +160,21 @@ def delete_product(product_id):
         return jsonify({"message": "Product deleted successfully"}), 200
 
     return jsonify({"message": "Product not found!"}), 404
+
+
+@app.route("/api/cart/add/<int:product_id>", methods=["POST"])
+@login_required
+def add_to_cart(product_id):
+    user = User.query.get(int(current_user.id))
+    product = Product.query.get(product_id)
+
+    if user and product:
+        cart_item = CartItem(user_id=user.id, product_id=product.id)
+        db.session.add(cart_item)
+        db.session.commit()
+        return jsonify({"message": "Product added to cart successfully"}), 200
+
+    return jsonify({"message": "User or product not found!"}), 404
 
 
 @app.route("/")
